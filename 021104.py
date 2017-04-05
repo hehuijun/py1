4dcb0f1a7238a212e4cd545e2c72fbbbd50c1a0f
hehuijun <he.huijun@gmail.com>
2017-04-05 08:46:22 +0800

    作练习题目

---
+++ 021104.py
@@ -0,0 +1,16 @@
+print((5>4) and (3==5))
+print('I think the result is "false"')
+
+print(not (5>4))
+print('I think the result is "Ture"')
+
+print((5>4) or (3==5))
+print('I think the result is "true"')
+print(not(5>4) or (3 == 5))
+print('I think the result is "false"')
+
+print((True and True) and (True == False))
+print('I think the result is "false"')
+
+print((not False) or (not True))
+print('I think the result is "false"')
